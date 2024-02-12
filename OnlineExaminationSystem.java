ff36af2877a987c98313f5e247b0661917d35ed1
Utkarsh <141557962+UtkarshBhardwaj1@users.noreply.github.com>
2024-02-12 21:22:10 +0530

    Update OnlineExaminationSystem.java

--- OnlineExaminationSystem.java
+++ OnlineExaminationSystem.java
@@ -1,35 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
-//creating class for users
-class User
-{
-    private String username;
-    private String password;
-    private String fullName;
-
-    public User(String username, String password, String fullName) //initializing user
-    {
-        this.username = username;
-        this.password = password;
-        this.fullName = fullName;
-    }
-
-    public String getUsername()
-    {
-        return username;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
-
-    public String getFullName()
-    {
-        return fullName;
+import java.util.Scanner;        return fullName;
     }
 }
 
