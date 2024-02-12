187b83ba30373116d46788d09bfc3502f5865873
Utkarsh <141557962+UtkarshBhardwaj1@users.noreply.github.com>
2024-02-12 21:22:38 +0530

    Update OnlineExaminationSystem.java

--- OnlineExaminationSystem.java
+++ OnlineExaminationSystem.java
@@ -3,60 +3,11 @@ import java.util.List;
 import java.util.Scanner;        return fullName;
     }
 }
-
-//question class for creating ques
-class Question
-{
-    private String questionText;
-    private List<String> options; //list to store questions
-    private int correctOptionIndex;
-
-    public Question(String questionText, List<String> options, int correctOptionIndex) //initializing question
-    {
-        this.questionText = questionText;
-        this.options = options;
-        this.correctOptionIndex = correctOptionIndex;
-    }
-
-    public String getQuestionText()
-    {
-        return questionText;
-    }
-
-    public List<String> getOptions()
-    {
-        return options;
-    }
-
-    public int getCorrectOptionIndex()
-    {
         return correctOptionIndex;
         
     }
     public class OnlineExaminationSystem
 {
-    private static User currentUser;
-    private static List<Question> questions;
-    private static List<Integer> selectedAnswers;
-    private static Timer timer;
-    private static int remainingTimeInSeconds = 1800; // 30 minutes
-
-    public static void main(String[] args)
-    {
-        initializeQuestions();
-        login();
-    }
-
-    private static void initializeQuestions() //storing question with options and answers in a list
-    {
-        questions = new ArrayList<>();
-        questions.add(new Question("What is 5^2?", List.of("10", "7", "3", "25"), 3));
-        questions.add(new Question("What is the capital of India?", List.of("Chennai", "Bengaluru", "Delhi", "Mumbai"), 2));
-        // here we can add more questions
-    }
-
-
-}
     private static void login() //function for user login
     {
         Scanner scanner = new Scanner(System.in);
